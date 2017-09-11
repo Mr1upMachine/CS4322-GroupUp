23dcbec8d21beb80b6cc3f0834878066728a1812
Sean Hale <seanhale1@live.com>
2017-09-11 17:32:22 -0400

    Login xml set + Main created

--- app/src/main/java/com/example/seanh/groupup/LoginActivity.java
+++ app/src/main/java/com/example/seanh/groupup/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.seanh.groupup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +12,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.textLoginForgot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"That sucks doesn't it?", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginActivity.this.startActivity( new Intent(LoginActivity.this, MainActivity.class) );
+            }
+        });
 
     }
 }
