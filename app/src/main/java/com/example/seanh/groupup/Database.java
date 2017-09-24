0cb32e6feac90dd2275277011b291254b2537a4c
Sean Hale <seanhale1@live.com>
2017-09-24 16:30:48 -0400

    Basic functionality working, read/write avail

--- app/src/main/java/com/example/seanh/groupup/Database.java
+++ app/src/main/java/com/example/seanh/groupup/Database.java
@@ -1,18 +1,59 @@
 package com.example.seanh.groupup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Database {
 
-    private static final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private static final DatabaseReference dataRoot = FirebaseDatabase.getInstance().getReference();
+    private static final DatabaseReference dataEvents = dataRoot.child("events");
+    private static final DatabaseReference dataEventIDs = dataEvents.child("id:");
+    private static int idCount = getIdCount();
 
-    public static void writeNewUser(String email, String password) {
-        User user = new User(email, password);
+    //TODO find better way to do this
+    private static Event tempEvent = new Event();
 
-        mDatabase.child("Users").setValue(user);
+    public static void recordNewEvent(Event e) {
+        e.setId(++idCount);
+        dataEventIDs.child("" + idCount).setValue(e);
+        setIdCount(idCount);
     }
-    public void setEmail(String email){
 
+    public static Event getEventById(int id){
+        // Read from the database
+        dataEventIDs.child(""+id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                tempEvent = dataSnapshot.getValue(Event.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+
+        return tempEvent;
+    }
+
+    public static int getIdCount(){
+        dataEvents.child("idCount").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                idCount = snapshot.getValue(Integer.class);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        return idCount;
+    }
+    public static void setIdCount(int id){
+        dataEvents.child("idCount").setValue(id);
     }
 
 }
